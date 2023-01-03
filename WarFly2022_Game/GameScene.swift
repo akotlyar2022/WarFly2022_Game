897b0d22fd2bbbe94575afd586729d9f9986923f
AleksandrKotliar <sashagsmnew@gmail.com>
2023-01-03 22:35:33 +0400

    remove all elements (islands, clouds) from scene

--- WarFly2022_Game/GameScene.swift
+++ WarFly2022_Game/GameScene.swift
@@ -25,7 +25,7 @@ class GameScene: SKScene {
     fileprivate func spawnClowds() {
         let spawnCloudWait = SKAction.wait(forDuration: 1)
         let spawnCloudAction = SKAction.run {
-            let cloud = Cloud.populate()
+            let cloud = Cloud.populate(at: nil)
             self.addChild(cloud)
         }
         
@@ -37,7 +37,7 @@ class GameScene: SKScene {
     fileprivate func spawnIslands() {
         let spawnIslandWait = SKAction.wait(forDuration: 2)
         let spawnIslandAction = SKAction.run {
-            let island = Island.populate()
+            let island = Island.populate(at: nil)
             self.addChild(island)
         }
         
@@ -84,6 +84,12 @@ class GameScene: SKScene {
             player.position.x = self.size.width + 70
         } else if player.position.x > self.size.width + 70 {
             player.position.x = -70
+        }
+        
+        enumerateChildNodes(withName: "backgroundSprite") { (node, stop) in
+            if node.position.y < -199 {
+                node.removeFromParent()
+            }
         }
     }
 }
