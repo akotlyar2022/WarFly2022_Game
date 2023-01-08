61a93bfada978c3ebf06a9b6f46d8b75f41d8616
AleksandrKotliar <sashagsmnew@gmail.com>
2023-01-09 01:09:52 +0400

    powerUP classes

---
+++ WarFly2022_Game/GreenPowerUp.swift
@@ -0,0 +1,21 @@
+//
+//  GreenPowerUp.swift
+//  WarFly2022_Game
+//
+//  Created by macbook on 09.01.23.
+//
+
+import SpriteKit
+
+class GreenPowerUp: PowerUp {
+    
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "BluePowerUp")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- WarFly2022_Game/PowerUp.swift
+++ WarFly2022_Game/PowerUp.swift
@@ -5,30 +5,33 @@
 //  Created by macbook on 04.01.23.
 //
 
-import UIKit
 import SpriteKit
 
+
 class PowerUp: SKSpriteNode {
-    let initialSize = CGSize(width: 52, height: 52)
-    let textureAtlas = SKTextureAtlas(named: "GreenPowerUp")
-    var animationSpriteArray = [SKTexture]()
+    fileprivate let initialSize = CGSize(width: 52, height: 52)
+    fileprivate let textureAtlas: SKTextureAtlas!
+    fileprivate var textureNameBeginsWith = ""
+    fileprivate var animationSpriteArray = [SKTexture]()
     
-    init() {
+    init(textureAtlas: SKTextureAtlas) {
         
-        let greenTexture = textureAtlas.textureNamed("missle_green_01")
-        super.init(texture: greenTexture, color: .clear, size: initialSize)
+        self.textureAtlas = textureAtlas
+        let textureName = textureAtlas.textureNames.sorted()[0]
+        let texture = textureAtlas.textureNamed(textureName)
+        textureNameBeginsWith = String(textureName.characters.dropLast(6))
+        super.init(texture: texture, color: .clear, size: initialSize)
+        self.setScale(0.7)
         self.name = "powerUp"
         self.zPosition = 20
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+
     
     func performRotation() {
         for i in 1...15 {
             let number = String(format: "%02d", i)
-            animationSpriteArray.append(SKTexture(imageNamed: "missle_green_\(number)"))
+            animationSpriteArray.append(SKTexture(imageNamed: textureNameBeginsWith + number.description))
         }
         
         SKTexture.preload(animationSpriteArray) {
@@ -36,5 +39,9 @@ class PowerUp: SKSpriteNode {
             let rotationForever = SKAction.repeatForever(rotation)
             self.run(rotationForever)
         }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
