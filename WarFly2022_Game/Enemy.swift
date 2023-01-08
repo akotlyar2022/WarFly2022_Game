0224916b1eaf474aac6a42fdaf83c0e6efbdfb2c
AleksandrKotliar <sashagsmnew@gmail.com>
2023-01-08 20:57:30 +0400

    creating class Enemy

---
+++ WarFly2022_Game/Enemy.swift
@@ -0,0 +1,27 @@
+//
+//  Enemy.swift
+//  WarFly2022_Game
+//
+//  Created by macbook on 08.01.23.
+//
+
+import SpriteKit
+
+class Enemy: SKSpriteNode {
+    
+    static var textureAtlas: SKTextureAtlas?
+    
+    init() {
+        let texture = Enemy.textureAtlas?.textureNamed("airplane_4ver2_13")
+        super.init(texture: texture, color: .clear, size: CGSize(width: 221, height: 204))
+        self.xScale = 0.5
+        self.yScale = -0.5
+        self.zPosition = 20
+        self.name = "sprite"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
